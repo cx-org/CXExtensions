ad0aebb384aea02850e508723af90da2cd9e709d
Xander Deng <dengxiang2010@gmail.com>
2019-09-12 11:31:58 +0800

    update(DelayedAutoCancellable): release canceller after cancel

--- Sources/CXExtensions/DelayedAutoCancellable.swift
+++ Sources/CXExtensions/DelayedAutoCancellable.swift
@@ -7,18 +7,18 @@ import CXFoundation
 extension Cancellable {
     
     public func cancel<S: Scheduler>(after interval: S.SchedulerTimeType.Stride, tolerance: S.SchedulerTimeType.Stride = .zero, scheduler: S, options: S.SchedulerOptions? = nil) -> DelayedAutoCancellable {
-        return DelayedAutoCancellable(cancel: self, after: interval, tolerance: tolerance, scheduler: scheduler, options: options)
+        return DelayedAutoCancellable(cancel: self.cancel, after: interval, tolerance: tolerance, scheduler: scheduler, options: options)
     }
 }
 
 public final class DelayedAutoCancellable: Cancellable {
     
-    private let canceller: Cancellable
+    private var cancelBody: (() -> Void)?
     
-    private var scheduleCanceller: Cancellable!
+    private var scheduleCanceller: Cancellable?
     
-    public init<S: Scheduler>(cancel: Cancellable, after interval: S.SchedulerTimeType.Stride, tolerance: S.SchedulerTimeType.Stride, scheduler: S, options: S.SchedulerOptions? = nil) {
-        self.canceller = cancel
+    public init<S: Scheduler>(cancel: @escaping () -> Void, after interval: S.SchedulerTimeType.Stride, tolerance: S.SchedulerTimeType.Stride, scheduler: S, options: S.SchedulerOptions? = nil) {
+        self.cancelBody = cancel
         // FIXME: we should schedule non-repeatedly, but it's not cancellable.
         self.scheduleCanceller = scheduler.schedule(after: scheduler.now.advanced(by: interval), interval: .seconds(.max), tolerance: tolerance, options: options) { [unowned self] in
             self.cancel()
@@ -26,7 +26,9 @@ public final class DelayedAutoCancellable: Cancellable {
     }
     
     public func cancel() {
-        scheduleCanceller.cancel()
-        canceller.cancel()
+        scheduleCanceller?.cancel()
+        scheduleCanceller = nil
+        cancelBody?()
+        cancelBody = nil
     }
 }
