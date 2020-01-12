af3742779669b8018a353f7ae9dbc69896f7fb35
Xander Deng <dengxiang2010@gmail.com>
2020-01-12 23:23:38 +0800

    refine DelayedAutoCancellable

--- Sources/CXExtensions/DelayedAutoCancellable.swift
+++ Sources/CXExtensions/DelayedAutoCancellable.swift
@@ -2,7 +2,7 @@ import CXShim
 
 extension Cancellable {
     
-    public func cancel<S: Scheduler>(after interval: S.SchedulerTimeType.Stride, tolerance: S.SchedulerTimeType.Stride = .zero, scheduler: S, options: S.SchedulerOptions? = nil) -> DelayedAutoCancellable {
+    public func cancel<S: Scheduler>(after interval: S.SchedulerTimeType.Stride, tolerance: S.SchedulerTimeType.Stride?, scheduler: S, options: S.SchedulerOptions? = nil) -> DelayedAutoCancellable {
         return DelayedAutoCancellable(cancel: self.cancel, after: interval, tolerance: tolerance, scheduler: scheduler, options: options)
     }
 }
@@ -13,10 +13,10 @@ public final class DelayedAutoCancellable: Cancellable {
     
     private var scheduleCanceller: Cancellable?
     
-    public init<S: Scheduler>(cancel: @escaping () -> Void, after interval: S.SchedulerTimeType.Stride, tolerance: S.SchedulerTimeType.Stride, scheduler: S, options: S.SchedulerOptions? = nil) {
+    public init<S: Scheduler>(cancel: @escaping () -> Void, after interval: S.SchedulerTimeType.Stride, tolerance: S.SchedulerTimeType.Stride? = nil, scheduler: S, options: S.SchedulerOptions? = nil) {
         self.cancelBody = cancel
         // FIXME: we should schedule non-repeatedly, but it's not cancellable.
-        self.scheduleCanceller = scheduler.schedule(after: scheduler.now.advanced(by: interval), interval: .seconds(.max), tolerance: tolerance, options: options) { [unowned self] in
+        self.scheduleCanceller = scheduler.schedule(after: scheduler.now.advanced(by: interval), interval: .seconds(.max), tolerance: tolerance ?? scheduler.minimumTolerance, options: options) { [unowned self] in
             self.cancel()
         }
     }
